aa60145582a692ce766a1a4fb5922a8b2dd35b10
10XYXYX10 <lmcr10100722@gmail.com>
2024-02-13 15:42:46 +0900

    lesson6_re

--- lib/functions/index.ts
+++ lib/functions/index.ts
@@ -1,10 +1,10 @@
-//「<,>,',",&」や「;」といった特別な役割を果たす単語を無害化
+//「「<>%"\'`;?&=」といった特別な役割を果たす単語を無害化
 export const validationForWord = (str:string,limit:number=20): [boolean, string] => {
     // 長さ制限
-    if (str.length === 0 || str.length > limit) return [false, `1～${limit}字以内の文字列を入力して下さい`];
+    if (str.length === 0 || str.length > limit)return [false,`1～${limit}字以内の文字列を入力して下さい`];
 	//htmlエンティティ
-	const pattern = /[<>%"`';]/;
-	if(pattern.test(str))return [false, '「<>%"\'`;」これらの文字列は使用不可です'];
+	const pattern = /[<>%"`';?&=]/;
+	if(pattern.test(str))return [false, '「<>%"\'`;?&=」これらの文字列は使用不可です'];
     // 成功!!
     return [true, ''];
 }
